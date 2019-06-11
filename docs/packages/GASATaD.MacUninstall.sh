88adf8cf679f7776458cff13a93a84db1e890cac
Leandro Rodríguez <leandroide@gmail.com>
2019-06-11 09:38:27 +0100

    Unnecessary files deleted

--- docs/packages/GASATaD.MacUninstall.sh
+++
@@ -1,9 +0,0 @@
-echo
-echo "Uninstalling brew..."
-echo "--------------------"
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
-
-echo
-echo "Removing GASATaD from the system"
-echo "--------------------------------"
-rm -rf /Applications/GASATaD.app
