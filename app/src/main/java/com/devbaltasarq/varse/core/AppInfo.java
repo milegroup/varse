fab3d54d480f18327f305d4e25fbeb8d91415095
baltasarq <baltasarq@gmail.com>
2023-02-20 14:33:17 +0100

    Version bumped

--- app/src/main/java/com/devbaltasarq/varse/core/AppInfo.java
+++ app/src/main/java/com/devbaltasarq/varse/core/AppInfo.java
@@ -7,9 +7,9 @@ package com.devbaltasarq.varse.core;
 /** The version information for this app. */
 public class AppInfo {
     public static final String NAME = "VARSE";
-    public static final String VERSION = "v1.8.2 20221123";
+    public static final String VERSION = "v1.8.3 20230220";
     public static final String AUTHOR = "MILE Group";
-    public static final String EDITION = "iLimpio";
+    public static final String EDITION = "Relojero";
     public static final String APP_EMAIL = "varse.mile@gmail.com";
 
     public static String asShortString()
