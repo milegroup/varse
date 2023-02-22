36b2004b9f8a77b1a6571ba13a07b07456899aa2
baltasarq <baltasarq@gmail.com>
2023-02-22 17:41:47 +0100

    Bumps version

--- app/src/main/java/com/devbaltasarq/varse/core/AppInfo.java
+++ app/src/main/java/com/devbaltasarq/varse/core/AppInfo.java
@@ -7,7 +7,7 @@ package com.devbaltasarq.varse.core;
 /** The version information for this app. */
 public class AppInfo {
     public static final String NAME = "VARSE";
-    public static final String VERSION = "v1.8.3 20230220";
+    public static final String VERSION = "v1.8.4 20230222";
     public static final String AUTHOR = "MILE Group";
     public static final String EDITION = "Relojero";
     public static final String APP_EMAIL = "varse.mile@gmail.com";
